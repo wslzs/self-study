c8766e15bdac675fec709ae393a73d6193cc7466
王诗林 <399038956@qq.com>
2019-08-17 10:50:42 +0800

    创建公共utils项目

---
+++ wsl-common-utils/src/main/java/com/wsl/utils/wslcommonutils/WslCommonUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.wsl.utils.wslcommonutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WslCommonUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WslCommonUtilsApplication.class, args);
+    }
+
+}
---
+++ wsl-common-utils/src/test/java/com/wsl/utils/wslcommonutils/WslCommonUtilsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wsl.utils.wslcommonutils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WslCommonUtilsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
