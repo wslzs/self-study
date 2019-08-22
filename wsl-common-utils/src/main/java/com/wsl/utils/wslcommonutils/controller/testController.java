8fd0fb79c2e20247e509641148e6b137cf6ecbe1
王诗林 <399038956@qq.com>
2019-08-22 22:23:40 +0800

    创建web统一返回类

--- wsl-common-utils/src/main/java/com/wsl/utils/wslcommonutils/controller/testController.java
+++ wsl-common-utils/src/main/java/com/wsl/utils/wslcommonutils/controller/testController.java
@@ -1,4 +1,13 @@
 package com.wsl.utils.wslcommonutils.controller;
 
+import com.wsl.utils.wslcommonutils.vo.ResultVO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/common/test")
 public class testController {
+    @GetMapping
+    public ResultVO test() {
+        return ResultVO.ok("查询成功！");
+    }
 }
--- wsl-common-utils/src/main/java/com/wsl/utils/wslcommonutils/vo/ResultVO.java
+++ wsl-common-utils/src/main/java/com/wsl/utils/wslcommonutils/vo/ResultVO.java
@@ -2,25 +2,39 @@ package com.wsl.utils.wslcommonutils.vo;
 
 import lombok.Data;
 
+import java.util.HashMap;
+
 /**
  * 统一web返回类
- * @param <T>
+ *
  * @author wsl
  */
 @Data
-public class ResultVO<T> {
-    private String status;
-
-    private String code;
-
-    private String msg;
-
-    private T resultBody;
+public class ResultVO extends HashMap<String, Object> {
 
     public ResultVO() {
+        put("code", 0);
+        put("msg", "操作成功");
     }
 
-    public ResultVO(T resultBody) {
-        this.resultBody = resultBody;
+    public static ResultVO error() {
+        return error(1, "操作失败");
+    }
+
+    public static ResultVO error(String msg) {
+        return error(500, msg);
+    }
+
+    public static ResultVO error(int code, String msg) {
+        ResultVO resultVO = new ResultVO();
+        resultVO.put("code", code);
+        resultVO.put("msg", msg);
+        return resultVO;
+    }
+
+    public static ResultVO ok(String msg) {
+        ResultVO resultVO = new ResultVO();
+        resultVO.put("msg", msg);
+        return resultVO;
     }
 }
